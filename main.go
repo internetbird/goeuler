00b6a8ba3ce9e175b2bdf9dd1376232cba4344f1
Shai Aharoni <shai_aharoni@hotmail.com>
2016-06-25 15:45:33 +0300

    Added problem #10

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 
-	euler1()
-	euler2()
+	//	euler1()
+	//	euler2()
+	euler10()
+
 }
 
 func euler1() {
@@ -40,5 +43,32 @@ func euler2() {
 
 	}
 
-	fmt.Println("The answer to Project Euler Problem #22 is :", sum)
+	fmt.Println("The answer to Project Euler Problem #2 is :", sum)
+}
+
+func euler10() {
+	sum := 0
+
+	for i := 2; i < 2000000; i++ {
+		if isPrime(i) {
+			sum += i
+		}
+	}
+	fmt.Println("The answer to Project Euler Problem #10 is :", sum)
+}
+
+func isPrime(num int) bool {
+
+	if num == 2 || num == 3 {
+		return true
+	}
+	checkLimit := int(math.Sqrt(float64(num)))
+
+	for i := 2; i <= checkLimit; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+
+	return true
 }
