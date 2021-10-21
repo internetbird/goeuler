7fdb67e83b555c25715dfa427786a0bdce226f34
Shai Aharoni <saharoni@gmail.com>
2021-10-21 21:31:35 +0300

    Problem 26 attempt

--- 20-29/26.go
+++ 20-29/26.go
@@ -2,11 +2,39 @@ package main
 
 import (
 	"fmt"
+	"math/big"
+	"strings"
 )
 
 func Euler26() {
-	for i := 1; i < 20; i++ {
-		fmt.Printf("\n -- 1/%d = %.20f", i, 1/float64(i))
+
+	for i := 2; i < 1000; i++ {
+
+		nominator := big.NewFloat(1)
+		denominator := big.NewFloat(float64(i))
+
+		fraction := new(big.Float).Quo(nominator, denominator)
+		fractionString := fraction.Text('f', 100)
+
+		fractionPart := fractionString[2:]
+
+		for j := 9; j < len(fractionPart)/2; j++ {
+
+			zerosString := strings.Repeat("0", j)
+
+			for offset := 0; offset < len(fractionPart)-j*2; offset++ {
+				part1 := fractionPart[offset : offset+j]
+				part2 := fractionPart[offset+j : offset+j*2]
+
+				if part1 == part2 && part1 != zerosString {
+					fmt.Printf("\n -- 1/%d = %s", i, fractionString)
+					fmt.Printf("\nFraction part is: %s", fractionPart)
+					fmt.Printf("\nPart 1: %s Part 2: %s", part1, part2)
+				}
+
+			}
+		}
+
 	}
 
 }
